fda943687058d378d407513fccb774ea4a977468
Alex Withington-Smith <alex@awithsmith.com>
2021-01-14 07:56:17 +0000

    added test for normal item degradation

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,5 +1,13 @@
-var {Shop, Item} = require('../src/gilded_rose.js');
-describe("Gilded Rose", function() {
+const { Shop, Item } = require('../src/gilded_rose.js');
+describe("Gilded Rose", () => {
+  describe("updateQuality()", () => {
+    it("Should reduce a positive quality on an in date 'normal' item by 1", () => {
+      let gildedRose = new Shop([ new Item("foo", 10, 10) ]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(9);
+    });
+  });
+
 
   it("should foo", function() {
     const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
