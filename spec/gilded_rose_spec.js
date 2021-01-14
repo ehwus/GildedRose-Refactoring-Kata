16d0d3e82f6080de5eeb55241986960c760d4e3f
Alex Withington-Smith <alex@awithsmith.com>
2021-01-14 07:58:29 +0000

    added test to ensure items are correctly added to the inventory

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,5 +1,12 @@
 const { Shop, Item } = require('../src/gilded_rose.js');
 describe("Gilded Rose", () => {
+  it("correctly add items to the inventory", () => {
+    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+    expect(gildedRose.items[0].name).toEqual("foo");
+    expect(gildedRose.items[0].sellIn).toEqual(0);
+    expect(gildedRose.items[0].quality).toEqual(0)
+  });
+
   describe("updateQuality()", () => {
     it("Should reduce a positive quality on an in date 'normal' item by 1", () => {
       let gildedRose = new Shop([ new Item("foo", 10, 10) ]);
@@ -7,12 +14,4 @@ describe("Gilded Rose", () => {
       expect(items[0].quality).toEqual(9);
     });
   });
-
-
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
-  });
-
 });
