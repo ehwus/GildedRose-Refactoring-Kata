37fca5f3bf2ec1cd6b270d2f2db9b2081df903b6
Alex Withington-Smith <alex@awithsmith.com>
2021-01-14 08:11:41 +0000

    added test for increased degradation after sell by date

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,7 +1,8 @@
 const { Shop, Item } = require('../src/gilded_rose.js');
+
 describe("Gilded Rose", () => {
   it("correctly add items to the inventory", () => {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+    let gildedRose = new Shop([ new Item("foo", 0, 0) ]);
     expect(gildedRose.items[0].name).toEqual("foo");
     expect(gildedRose.items[0].sellIn).toEqual(0);
     expect(gildedRose.items[0].quality).toEqual(0)
@@ -16,6 +17,12 @@ describe("Gilded Rose", () => {
 
     it("Cannot reduce quality below 0 on normal item", () => {
       let gildedRose = new Shop([ new Item("foo", 10, 0) ]);
+      let items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(0);
+    });
+
+    it("Degrades item quality twice as fast after sell by has passed", () => {
+      let gildedRose = new Shop([ new Item("foo", 0, 2) ]);
       let items = gildedRose.updateQuality();
       expect(items[0].quality).toEqual(0);
     });
